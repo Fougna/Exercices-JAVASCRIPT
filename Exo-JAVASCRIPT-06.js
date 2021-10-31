21a2e0b6ef57670b2f1a43bf00b1ba6f37899312
Sébastien Ferran <sebferran@gmail.com>
2021-10-31 23:15:10 +0100

    Ajout Exercice JavaScript n°5

---
+++ Exo-JAVASCRIPT-06.js
@@ -0,0 +1,10 @@
+/* Exercice JavaScript 06 (Conçu par Amin Mangeot)
+
+Créez une variable playlist contenant “Katy Perry - Chained To The Rhythm” , "Ed Sheeran - Shape of You" , "Maroon 5 - Don't Wanna Know".
+En utilisant la variable playlist définissez une condition qui affiche dans la console le message suivant "Attention il ne vous reste plus que 3 titres" s'il y a un nombre de titres inférieur à 4.*/
+
+var playlist = ["Katy Perry - Chained To The Rhythm", "Ed Sheeran - Shape of You", "Maroon 5 - Don't Wanna Know"];
+
+if (playlist.length<4){
+    console.log("Attention, il ne vous reste plus que 3 titres !");
+};
