eded18086af7cd6f6eab9c6003192a161f0f838f
Sébastien Ferran <sebferran@gmail.com>
2021-11-03 12:20:45 +0100

    Ajout Exercice JavaScript n°22

--- Exo-JAVASCRIPT-22.js
+++ Exo-JAVASCRIPT-22.js
@@ -2,3 +2,17 @@
 
 Écrire une fonction qui affiche une boîte de dialogue alert les entiers compris entre les entiers d et f.
 Les valeurs de d et f sont saisies au clavier lors de l’exécution.*/
+
+var resultat = "";
+
+function difference (d,f){
+    for (i=d+1 ; i<f ; i++){
+        resultat = resultat + [i];
+    }
+    alert ("Les nombres compris entre " + d + " et " + f + " sont :" + resultat);
+};
+
+var d = prompt("Veuillez entrer un petit nombre...");
+var f = prompt("Veuillez entrer un grand nombre...");
+
+difference(parseInt(d), parseInt(f));
