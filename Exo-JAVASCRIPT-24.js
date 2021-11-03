114a3da86eba09cb9fbf22405799bc3ad5998889
Sébastien Ferran <sebferran@gmail.com>
2021-11-03 18:29:38 +0100

    Ajout Exercice JavaScript n°25

--- Exo-JAVASCRIPT-24.js
+++ Exo-JAVASCRIPT-24.js
@@ -5,9 +5,27 @@ Créez une fonction
 * qui prend une variable qui s'appelle .
 * qui vérifie si l'âge est supérieur à 18 ans.
 * Si Oui il indique majeure si non.
-* il indique mineur et précise dans combien de temps il aura 18 ans.
+* il indique mineur et précise dans combien de temps il aura 18 ans.*/
 
-* Bonus : Afficher l'ensemble des nombre entre l'âge inférieur à 18 ans et 18 ans.
+var age = prompt("Veuillez rentrer votre âge...");
+
+function checkage(age){
+    if (age>18){
+        alert("Bravo ! Vous êtes majeur(e).");
+    }
+    else{
+        var majeur = 18-age;
+        alert("Vous êtes encore mineur(e) !" +
+        "\n mais patience vous deviendrez majeur(e) dans " + majeur + " ans.")
+    }
+    for (i=age ; i<18 ; i++){
+        alert("Années restantes avant la majorité :" + i);
+    }
+};
+
+checkage(parseInt(age));
+
+/* Bonus : Afficher l'ensemble des nombres entre l'âge inférieur à 18 ans et 18 ans.
 * Ex :
 * checkage(15)
 * Vous êtes mineurs
--- Exo-JAVASCRIPT-25.js
+++ Exo-JAVASCRIPT-25.js
@@ -8,10 +8,52 @@ Vous l'affichez dans une alert ou dans la log.
 Créez un objet avec les mêmes informations
 Vous l'affichez dans une alert ou dans la log.*/
 
+var nom = "Ferran", age = 45;
+alert(nom + ", " + age);
+
+var id = {
+    nom : "Ferran",
+    age : 45
+};
+alert(id.nom + id.age);
+
 /*2. Les conditions (if)
 Vérifier que l'âge est > 18 et < 100 SI c'est ok on affiche dans la log OK si non KO*/
 
+var age = prompt("Quel âge avez-vous ?");
+
+if (age>=18 && age<=100){
+    alert("OK !");
+}
+else{
+    alert("KO !");
+};
+
 /*3. Les boucles
-1 Afficher les nombre de 5 à 15.
+1 Afficher les nombres de 5 à 15.
 2 Afficher le symbole "" de 5 à 15 fois.
 3 Demandez à l'utilisateur un nombre entre 1 et 30 et afficher ce nombre de fois le symbole ""*/
+
+var num1 = 5;
+var num2 = 15;
+
+for (i=num1 ; i<num2+1 ; i++){
+    resultat = i;
+    alert(resultat + ' " "');
+};
+
+var user = prompt("Tapez un nombre entre 1 et 30...");
+
+function numerotation(user){
+    if(user>=1 && user<=30){
+        for (i=1 ; i<user+1 ; i++){
+            quotes = [i] + ' " "';
+            alert(quotes);
+        };
+    }else{
+        alert("La saisie est incorrecte.");
+        user = prompt("Tapez un nombre entre 1 et 30...");
+    }
+}
+
+numerotation(parseInt(user));
