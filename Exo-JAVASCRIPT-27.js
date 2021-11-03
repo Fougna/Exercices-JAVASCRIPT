eb6a2a8318fbad31f07c3f25c835a898b848804f
Sébastien Ferran <sebferran@gmail.com>
2021-11-04 00:19:43 +0100

    Ajout Exercice JavaScript n°27

--- Exo-JAVASCRIPT-27.js
+++ Exo-JAVASCRIPT-27.js
@@ -14,8 +14,8 @@ function etoile(nb){
 
 etoile(parseInt(nb));
 
-/*2. Objectif : Lorsque l’on clique sur le bouton « envoyer » de la page, nous voulons vérifier que les champs « texte » ne soient pas vides.
-Lorsque ces champs sont vides, affichez cette information dans la page html.
+/*2. Objectif : Lorsque l’on clique sur le bouton « envoyer » de la page, nous voulons vérifier que le champ « texte » ne soit pas vide.
+Lorsque ce champs est vide, affichez cette information dans la page html.
 (si nécessaire, il est possible d’ajouter des ‘id’ dans le code  html, pour simplifier le code js.)
     A.	Expliquez les étapes pour aboutir à ce résultat reprenant les principes suivants :
         I.	Comment implémenter l’évènement.
@@ -25,17 +25,19 @@ Lorsque ces champs sont vides, affichez cette information dans la page html.
             3.  Accéder à quel partie du DOM.
     B.	Écrivez le code correspondant en utilisant le script créé précédemment. */
 
-document.getElementById("envid").addEventListener("click", sender, false);
-
-function sender(){
-    if (document.getElementById("textid").value=""){
-        document.getElementById("divid").innerHTML="Le champ n'est pas renseigné.";
-        alert("Le champ n'est pas renseigné.");
-    };
-}
-
-
 /*Bonus :
 Créez une fonction que l’on pourra appeler de la façon suivante : factorielle(x) ; X correspondant à un nombre.
-Lorsque cette fonction sera appeléE, elle retournera la factorielle de X qui sera récupéré dans une variable puis affiché dans la log le résultat.
+Lorsque cette fonction sera appelée, elle retournera la factorielle de X qui sera récupéré dans une variable puis affiché dans la log le résultat.
 : Ex. de factorielle de 5 : 1*2*3*4*5.""*/
+
+function factorielle (entier){
+    resultat = 1;
+    for (i=1 ; i<entier ; i++){
+        resultat = resultat*i;
+        alert ("La factorielle de " + entier + " est : " + resultat);
+    }
+};
+
+var entier = prompt("Veuillez entrer un nombre entier positif afin d'en connaître la factorielle...");
+
+factorielle(parseInt(entier));
